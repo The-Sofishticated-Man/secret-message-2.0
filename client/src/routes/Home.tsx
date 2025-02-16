dcaa59c2979d493f844cd92553e9f65c7911d20d
Islam lameche <lameche1islam@gmail.com>
2025-02-16 17:43:51 +0100

    installed tailwind and added simple homepage with navbar

---
+++ client/src/routes/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+const Home = () => {
+  return (
+    <header className=" bg-blue-600">
+      <NavBar />
+    </header>
+  );
+};
+
+export default Home;
